7ddb0fe00287774780d2b73ede8f5bb64f91da2c
Vania Dimitrova <vani4ka66@abv.bg>
2017-09-12 11:16:44 +0300

    Create 01. Person.cs

---
+++ 05. Inheritance/01. Person.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace August2017
+{
+
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public virtual string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Name's length should not be less than 3 symbols!");
+                }
+                name = value;
+            }
+        }
+
+        public virtual int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age must be positive!");
+                }
+                age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Name: {this.Name}, Age: {this.Age}");
+            return sb.ToString();
+        }
+
+    }
+
+    public class Child : Person
+    {
+        public Child(string name, int age)
+            : base(name, age)
+        {
+
+        }
+
+        public override int Age
+        {
+            get { return base.Age; }
+            set
+            {
+                if (value > 15)
+                {
+                    throw new ArgumentException("Child's age must be less than 15!");
+                }
+                base.Age = value;
+            }
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Child child = new Child(name, age);
+                Console.WriteLine(child);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+
+        }
+    }
+}
